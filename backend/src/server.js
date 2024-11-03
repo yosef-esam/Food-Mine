3010885daa3105bdbd37f2a85bc170dcbcf6bc70
yosef esam <150367164+yosef-esam@users.noreply.github.com>
2024-11-03 23:02:20 +0200

    ff

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(
   cors({
     origin:
-      "https://food-app-client-kohl.vercel.app/" || "http://localhost:5173",
+      "https://food-app-client-kohl.vercel.app" || "http://localhost:5173",
     credentials: true,
   })
 );
