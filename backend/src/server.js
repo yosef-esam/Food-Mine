212d127ec8bd9611e96582c4db8027dccc955bed
yosef esam <150367164+yosef-esam@users.noreply.github.com>
2024-11-05 16:45:42 +0200

    fffff

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,8 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: " http://localhost:5173",
+    origin:
+      " http://localhost:5173" || "https://food-app-client-kohl.vercel.app",
     credentials: true,
   })
 );
--- frontend/src/axiosconfig.js
+++ frontend/src/axiosconfig.js
@@ -1,3 +1,5 @@
 import axios from "axios";
 axios.defaults.baseURL =
-  process.env.NODE_ENV !== "production" ? "http://localhost:5000" : "/";
+  process.env.NODE_ENV !== "production"
+    ? "https://food-app-server-ff.vercel.app/"
+    : "/";
