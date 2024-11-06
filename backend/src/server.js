eb8ea96013022e47dcfdf3022d301f75f94ba5be
yosef esam <150367164+yosef-esam@users.noreply.github.com>
2024-11-06 20:53:41 +0200

    ffff

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(
   cors({
     origin:
-      " http://localhost:5173" || "https://food-app-dusky-nine.vercel.app/",
+      " http://localhost:5173" || "https://food-app-dusky-nine.vercel.app",
     credentials: true,
   })
 );
