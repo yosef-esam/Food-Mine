846b7802a2bd7fc8b6375f8b4b8924822d8dd35c
yosef esam <150367164+yosef-esam@users.noreply.github.com>
2024-11-07 16:25:21 +0200

    fffc

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,10 +14,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: [
-      " http://localhost:5173",
-      "https://food-app-client-kappa.vercel.app",
-    ],
+    origin: " http://localhost:5173",
     credentials: true,
   })
 );
