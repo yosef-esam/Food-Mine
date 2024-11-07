e1226d5cada24c0b40988032bc585830949c915e
yosef esam <150367164+yosef-esam@users.noreply.github.com>
2024-11-07 16:17:11 +0200

    cvv

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(
   cors({
     origin:
-      " http://localhost:5173" || "https://food-app-client-kappa.vercel.app",
+      " http://localhost:5173" || "https://food-app-client-kappa.vercel.app/",
     credentials: true,
   })
 );
