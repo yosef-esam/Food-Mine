c4a0c58e8271265daac1c1b712500446798b84ef
yosef esam <150367164+yosef-esam@users.noreply.github.com>
2025-02-16 15:28:15 +0200

    ff

--- frontend/src/axiosconfig.js
+++ frontend/src/axiosconfig.js
@@ -2,4 +2,4 @@ import axios from "axios";
 axios.defaults.baseURL =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000"
-    : "https://food-app-server-972aj060i-yosef-esams-projects.vercel.app/api/foods";
+    : "https://food-app-server-ff.vercel.app/";
