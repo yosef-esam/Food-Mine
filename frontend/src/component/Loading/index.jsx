161ec0037b4890d873687ae63194b955828b0efd
yosef esam <150367164+yosef-esam@users.noreply.github.com>
2025-04-23 14:48:37 +0200

    dd

--- frontend/src/component/Loading/index.jsx
+++ frontend/src/component/Loading/index.jsx
@@ -8,10 +8,10 @@ function Loading() {
   }
   return (
     <div>
-      <div className=" fixed w-full h-full bg-white z-50  top-0 left-0 ">
+      <div className=" fixed w-full h-full bg-[#ededee] z-50  top-0 left-0 ">
         <div className=" flex items-center justify-center flex-col  h-[80%] w-[80#]">
           <img
-            src="https://media.geeksforgeeks.org/wp-content/uploads/20210722235713/sw.gif"
+            src="https://medihttps://usagif.com/wp-content/uploads/loading-81.gif"
             alt=""
           />
         </div>
